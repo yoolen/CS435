fa4b53808390ee3fde6e1fe8bd7f9ac5c70fb866
Ulenn Terry Chern <utc2@njit.edu>
2015-10-24 02:24:03 -0400

    added addNode functionality (similar to insert but instead of replacing updates the value)

--- src/project/Matrix.java
+++ src/project/Matrix.java
@@ -16,6 +16,50 @@ public class Matrix{
 	public int getSize(){
 		return this.size;
 	}
+	
+	public Node[] getRowhead(){
+		return this.rowhead;
+	}
+	
+	public Node[] getColhead(){
+		return this.colhead;
+	}
+	
+	/*	Add node takes a node and adds it to the matrix in the correct row and column
+	 *  Preconditions: addNode is only executed on a non-empty row (null rows are handled elsewhere)
+	 *  Postconditions: if a node does not exist it will be added to the matrix; if it already
+	 *  exists the value of the inserted node will be added to the current node.
+	 */ 
+	public void addNode(Node node){
+		Node curr = rowhead[node.getR()];			// need to traverse the row until at correct location
+		while(curr.getNextr() != rowhead[node.getR()] && node.getC() < curr.getC()){
+			curr = curr.getNextr();				// if we reach the end, append to the end
+		}
+		if(curr.getC()==node.getC()){ 			// if duplicate location is found then we add the
+			curr.setValue(curr.getValue() + node.getValue()); 	// value of the current node to that of the inserted
+			node = null;						// node and clear the node
+		} else {
+			node.setNextr(curr.getNextr());		// otherwise simply insert the node
+			curr.setNextr(node);
+		}
+	
+		if(node == null){
+			// we found a duplicate and updated the node value before so just do nothing
+		} else {
+			// deal with columns
+			if(colhead[node.getC()] == null){			// if the head pointer is null (empty col so far)
+				colhead[node.getC()] = node;
+				node.setNextc(node);
+			} else {
+				curr = colhead[node.getC()];			// need to traverse the row until at correct location
+				while(curr.getNextc() != colhead[node.getC()] && node.getR() < curr.getR()){
+					curr = curr.getNextc();				// if we reach the end, append to the end
+				}
+				node.setNextc(curr.getNextc());
+				curr.setNextc(node);
+			}
+		}
+	}
 
 	public void insertNode(Node node){
 		if (node.getValue() == 0) {
@@ -28,7 +72,7 @@ public class Matrix{
 				node.setNextr(node);					// node and then point the node back to itself.
 			} else {
 				curr = rowhead[node.getR()];			// need to traverse the row until at correct location
-				while(curr.getNextr() != rowhead[node.getR()] && node.getC() < curr.getC()){
+				while(curr.getNextr() != rowhead[node.getR()] && curr.getC() < node.getC()){
 					curr = curr.getNextr();				// if we reach the end, append to the end
 				}
 				if(curr.getC()==node.getC()){ 			// if duplicate location is found then we set the
@@ -48,7 +92,7 @@ public class Matrix{
 					node.setNextc(node);
 				} else {
 					curr = colhead[node.getC()];			// need to traverse the row until at correct location
-					while(curr.getNextc() != colhead[node.getC()] && node.getR() < curr.getR()){
+					while(curr.getNextc() != colhead[node.getC()] && curr.getR() < node.getR()){
 						curr = curr.getNextc();				// if we reach the end, append to the end
 					}
 					node.setNextc(curr.getNextc());
@@ -74,11 +118,44 @@ public class Matrix{
 		}
 		return array;
 	}
+	
+	public int[][] Matrix2Arraybycol(){ // testing to see if linking is done properly for columns
+		//		if(this.rowhead == null || this.colhead == null){
+		//			return null;
+		//		}
+		int[][] array = new int[size][size];
+		for(int i = 0; i < size; i++){
+			Node parser = this.colhead[i];
+			if(parser == null){ continue; }
+			while(parser.getNextc() != this.colhead[i]){
+				array[parser.getR()][i] = parser.getValue();
+				parser = parser.getNextc();
+			}	
+			array[parser.getR()][i] = parser.getValue();
+		}
+		return array;
+	}
 
 
 	@Override
 	public String toString(){ // Print method
 		int[][] array = this.Matrix2Array();
+		String output = "";
+		if(array == null){ output = "Empty array"; }
+		else{
+			for(int i = 0; i < size; i++){
+				output += "[";
+				for(int j = 0; j < size; j++){
+					output += String.format("%5d", array[i][j]);
+				}
+				output += "  ]\n";
+			}
+		}
+		return output;
+	}
+	
+	public String printbycol(){ // Print method
+		int[][] array = this.Matrix2Arraybycol();
 		String output = "";
 		if(array == null){ output = "Empty array"; }
 		else{
