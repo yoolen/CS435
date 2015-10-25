26d1b8ded7dec794160ab47b9bc9aa718e89f7bb
Ulenn Terry Chern <utc2@njit.edu>
2015-10-24 21:04:22 -0400

    Added scalar multiply, addition, and subtraction methods. Included an initialize by default method.

--- src/project/Matrix.java
+++ src/project/Matrix.java
@@ -25,40 +25,47 @@ public class Matrix{
 		return this.colhead;
 	}
 	
-	/*	Add node takes a node and adds it to the matrix in the correct row and column
-	 *  Preconditions: addNode is only executed on a non-empty row (null rows are handled elsewhere)
-	 *  Postconditions: if a node does not exist it will be added to the matrix; if it already
-	 *  exists the value of the inserted node will be added to the current node.
-	 */ 
+
 	public void addNode(Node node){
-		Node curr = rowhead[node.getR()];			// need to traverse the row until at correct location
-		while(curr.getNextr() != rowhead[node.getR()] && node.getC() < curr.getC()){
-			curr = curr.getNextr();				// if we reach the end, append to the end
-		}
-		if(curr.getC()==node.getC()){ 			// if duplicate location is found then we add the
-			curr.setValue(curr.getValue() + node.getValue()); 	// value of the current node to that of the inserted
-			node = null;						// node and clear the node
+		if (node.getValue() == 0) {
+			// do nothing
 		} else {
-			node.setNextr(curr.getNextr());		// otherwise simply insert the node
-			curr.setNextr(node);
-		}
-	
-		if(node == null){
-			// we found a duplicate and updated the node value before so just do nothing
-		} else {
-			// deal with columns
-			if(colhead[node.getC()] == null){			// if the head pointer is null (empty col so far)
-				colhead[node.getC()] = node;
-				node.setNextc(node);
-			} else {
-				curr = colhead[node.getC()];			// need to traverse the row until at correct location
-				while(curr.getNextc() != colhead[node.getC()] && node.getR() < curr.getR()){
-					curr = curr.getNextc();				// if we reach the end, append to the end
+			Node curr;
+			// start with rows
+			if(rowhead[node.getR()] == null){			// if the head pointer is null (empty row so far)
+				rowhead[node.getR()] = node;			// make the row header for that row point to the
+				node.setNextr(node);					// node and then point the node back to itself.
+			} else { 
+				curr = rowhead[node.getR()];			// need to traverse the row until at correct location
+				while(curr.getNextr() != rowhead[node.getR()] && curr.getC() < node.getC()){
+					curr = curr.getNextr();				// if we reach the end, append to the end
 				}
-				node.setNextc(curr.getNextc());
-				curr.setNextc(node);
+				if(curr.getC()==node.getC()){ 			// if duplicate location is found then we add the
+					curr.setValue(curr.getValue() + node.getValue()); 	// value of the current node to that of the inserted
+					node = null;						// node and clear the node
+				} else {
+					node.setNextr(curr.getNextr());		// otherwise simply insert the node
+					curr.setNextr(node);
+				}
 			}
-		}
+
+			if(node == null){
+				// we found a duplicate and updated the node value before so just do nothing
+			} else {
+				// deal with columns
+				if(colhead[node.getC()] == null){			// if the head pointer is null (empty col so far)
+					colhead[node.getC()] = node;
+					node.setNextc(node);
+				} else {
+					curr = colhead[node.getC()];			// need to traverse the row until at correct location
+					while(curr.getNextc() != colhead[node.getC()] && node.getR() < curr.getR()){
+						curr = curr.getNextc();				// if we reach the end, append to the end
+					}
+					node.setNextc(curr.getNextc());
+					curr.setNextc(node);
+				}
+			}
+		} 
 	}
 
 	public void insertNode(Node node){
