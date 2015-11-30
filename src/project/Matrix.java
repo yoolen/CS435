adfe8d19773b6b5ce8105fcef478a451822f400d
Ulenn Terry Chern <utc2@njit.edu>
2015-11-30 00:24:39 -0500

    fixing add node

--- src/project/Matrix.java
+++ src/project/Matrix.java
@@ -24,50 +24,6 @@ public class Matrix{
 	public Node[] getColhead(){
 		return this.colhead;
 	}
-	
-
-/*	public void addNode(Node node){
-		if (node.getValue() == 0) {
-			// do nothing
-		} else {
-			Node curr;
-			// start with rows
-			if(rowhead[node.getR()] == null){			// if the head pointer is null (empty row so far)
-				rowhead[node.getR()] = node;			// make the row header for that row point to the
-				node.setNextr(node);					// node and then point the node back to itself.
-			} else { 
-				curr = rowhead[node.getR()];			// need to traverse the row until at correct location
-				while(curr.getNextr() != rowhead[node.getR()] && curr.getC() < node.getC()){
-					curr = curr.getNextr();				// if we reach the end, append to the end
-				}
-				if(curr.getNextr().getC()==node.getC()){ 	// if duplicate location is found then we check if the sum is 0
-					if(curr.getNextr().getValue() + node.getValue() == 0){	// remove the node
-					curr.setValue(curr.getValue() + node.getValue()); 	// value of the current node to that of the inserted
-					node = null;						// node and clear the node
-				} else {
-					node.setNextr(curr.getNextr());		// otherwise simply insert the node
-					curr.setNextr(node);
-				}
-			}
-
-			if(node == null){
-				// we found a duplicate and updated the node value before so just do nothing
-			} else {
-				// deal with columns
-				if(colhead[node.getC()] == null){			// if the head pointer is null (empty col so far)
-					colhead[node.getC()] = node;
-					node.setNextc(node);
-				} else {
-					curr = colhead[node.getC()];			// need to traverse the row until at correct location
-					while(curr.getNextc() != colhead[node.getC()] && curr.getR() < node.getR()){
-						curr = curr.getNextc();				// if we reach the end, append to the end
-					}
-					node.setNextc(curr.getNextc());
-					curr.setNextc(node);
-				}
-			}
-		} 
-	}*/
 
 	public void insertNode(Node node){
 		if (node.getValue() == 0) {
